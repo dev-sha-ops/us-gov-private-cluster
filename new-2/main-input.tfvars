73638cf1a44a5f93aed8daa6ee38ae2fcd4bda0b
Shadab Shah <shahshadab1680@gmail.com>
2025-01-24 03:13:09 +0500

    Add Code

---
+++ new-2/main-input.tfvars
@@ -0,0 +1,11 @@
+region = "eu-central-1"
+### Cluster
+eks_cluster_name = "sample-cluster-01"
+eks_cluster_version = "1.31"
+node_host_key_name = "default"
+### Linux Nodegroup
+lin_desired_size = "2"
+lin_max_size = "2"
+lin_min_size = "2"
+lin_instance_type = "t3.medium"
+bucket         = "waggsinfra-backend-bucket"
---
+++ new-2/main-var.tf
@@ -0,0 +1,33 @@
+variable "region" {
+  description = "Please enter the region used to deploy this infrastructure"
+  type        = string
+}
+variable "eks_cluster_version" {
+  description = "Please enter the EKS cluster version"
+  type        = string
+}
+variable "eks_cluster_name" {
+  description = "Please enter an EKS cluster name"
+  type        = string
+}
+variable "lin_instance_type" {
+  description = "Please enter the instance type to be used for the Linux worker nodes"
+  type        = string
+}
+variable "lin_min_size" {
+  description = "Please enter the minimal size for the Linux ASG"
+  type        = string
+}
+variable "lin_max_size" {
+  description = "Please enter the maximal size for the Linux ASG"
+  type        = string
+}
+variable "lin_desired_size" {
+  description = "Please enter the desired size for the Linux ASG"
+  type        = string
+}
+
+variable "node_host_key_name" {
+  description = "Please enter the name of the SSH key pair that should be assigned to the worker nodes of the cluster"
+  type        = string
+}
